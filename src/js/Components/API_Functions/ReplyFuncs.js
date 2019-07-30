cd532eca1cd0918be2346a0c69932592f99674d9
huycans <huycan_lk@yahoo.com.vn>
2019-07-30 17:36:26 +0700

    Implement post reply to a thread

--- src/js/Components/API_Functions/ReplyFuncs.js
+++ src/js/Components/API_Functions/ReplyFuncs.js
@@ -8,10 +8,32 @@ async function sendReply(authData, content, threadId) {
 				id: authData.userId
 			},
 			threadId: threadId,
-			content: content
-		};
-
-		await sendDataWithAuth("PUT", SERVER_API.reply, authData, body);
+      content: content,
+      // token: authData.sessionToken
+    };
+		const link = URL + SERVER_API.reply;
+    const bearer = 'Bearer ' + authData.sessionToken;
+    let serverResponse = await fetch(link, {
+			method: "POST",
+      withCredentials: true,
+			headers: {
+				Accept: "application/json",
+				"Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+        "Authorization": bearer
+			},
+			body: JSON.stringify(body)
+    });
+    let responseJSON = await serverResponse.json();
+		console.log(responseJSON);
+		if (serverResponse.status === 200) {
+			//make sure there is content inside the response before return it
+			return responseJSON;
+		} else {
+			console.log(responseJSON.message);
+			throw responseJSON.message;
+		}
+		// await sendDataWithAuth("PUT", SERVER_API.reply, authData, body);
 	} catch (error) {
 		throw error;
 	}
